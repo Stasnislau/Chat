ce8002cb870c4bf5a050a1c89b908762a40f3070
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-09-07 16:14:20 +0200

    implemented user logic

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,8 +1,54 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Body, Controller, Param, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { userDTO } from "./dto";
+import ApiError from "src/exceptions/api-error";
 
-@Controller('user')
+@Controller("user")
 export class UserController {
-    constructor( private userService: UserService) {}
-    
+  constructor(private userService: UserService) {}
+
+  @Post("create")
+  async createUser(@Body() data: userDTO) {
+    const user = await this.userService.createUser(data);
+    if (user instanceof ApiError) {
+      throw user;
+    }
+    return user;
+  }
+
+  @Post("getById/:id")
+  async getUserById(@Param("id") id: string) {
+    const user = await this.userService.getUserById(id);
+    if (user instanceof ApiError) {
+      throw user;
+    }
+    return user;
+  }
+
+  @Post("getByNickname/:nickname")
+  async getUserByNickname(@Param("nickname") nickname: string) {
+    const user = await this.userService.getUserByNickname(nickname);
+    if (user instanceof ApiError) {
+      throw user;
+    }
+    return user;
+  }
+
+  @Post("delete/:id")
+  async deleteUser(@Param("id") id: string) {
+    const user = await this.userService.deleteUser(id);
+    if (user instanceof ApiError) {
+      throw user;
+    }
+    return user;
+  }
+
+  @Post("update/:id")
+  async updateUser(@Param("id") id: string, @Body() data: userDTO) {
+    const user = await this.userService.updateUser(id, data);
+    if (user instanceof ApiError) {
+      throw user;
+    }
+    return user;
+  }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,8 +1,89 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import ApiError from "src/exceptions/api-error";
+import { userDTO } from "./dto";
 
 @Injectable()
 export class UserService {
-    constructor(private prisma : PrismaService) {}
-    
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: userDTO) {
+    const ifExist = await this.prisma.user.findUnique({
+      where: {
+        nickname: data.nickname,
+      },
+    });
+    if (ifExist) {
+      return ApiError.badRequest("User already exist");
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        nickname: data.nickname,
+      },
+    });
+    if (!user) {
+      return ApiError.badRequest("User not created");
+    }
+    return {
+      id: user.id,
+    };
+  }
+
+  async getUserById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      return ApiError.badRequest("User not found");
+    }
+    return user;
+  }
+
+  async getUserByNickname(nickname: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        nickname,
+      },
+    });
+    if (!user) {
+      return ApiError.badRequest("User not found");
+    }
+    return user;
+  }
+
+  async deleteUser(id: string) {
+    const user = await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      return ApiError.badRequest("User not found");
+    }
+    return {
+      id: user.id,
+    };
+  }
+
+  async updateUser(id: string, data: userDTO) {
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name: data.name,
+        nickname: data.nickname,
+      },
+    });
+    if (!user) {
+      return ApiError.badRequest("User not found");
+    }
+    return {
+      id: user.id,
+    };
+  }
+
 }
