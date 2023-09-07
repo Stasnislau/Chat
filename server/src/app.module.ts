142d44ecbd1dc0c5227bb2b86da6551f03794a24
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-09-07 19:05:57 +0200

    minor fixes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ChatGateway } from './chat-gateway/chat.gateway';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { MessageController } from './message/message.controller';
 import { MessageModule } from './message/message.module';
-import { RoomService } from './room/room.service';
 import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [PrismaModule, UserModule, MessageModule, RoomModule],
-  controllers: [AppController, MessageController],
-  providers: [AppService, ChatGateway, ChatGateway, PrismaService, RoomService],
+  controllers: [AppController],
+  providers: [ChatGateway],
 })
 export class AppModule {}
