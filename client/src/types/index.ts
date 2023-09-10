0bb885bed548066d4ce78bb6e30e197dded20227
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-09-10 14:45:05 +0200

    updated types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,5 +1,8 @@
-export interface alertInterface{
-    type: "success" | "error";
-    text: string;
-    id: number;
+export interface alertInterface {
+  type: "success" | "error";
+  text: string;
+  id: number;
+}
+export interface fileObject extends File {
+  preview: string;
 }
