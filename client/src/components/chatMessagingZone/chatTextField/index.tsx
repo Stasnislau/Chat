6ba2a11b48c32518f9a11de56a8efa1b5ea8b711
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-09-23 20:19:20 +0200

    deleted usage of library

--- client/src/components/chatMessagingZone/chatTextField/index.tsx
+++ client/src/components/chatMessagingZone/chatTextField/index.tsx
@@ -7,20 +7,18 @@ import {
 } from "@mui/material";
 import { Send, Mic } from "@mui/icons-material";
 import { Context } from "../../../App";
-import RecordRTC from "recordrtc";
+import { set } from "mobx";
 interface ChatTextFieldProps {
   onSend: (text: string, room: string) => void;
   onRecord: (recording: Blob) => void;
 }
-
 const ChatTextField = ({ onSend, onRecord }: ChatTextFieldProps) => {
   const [isRecording, setIsRecording] = useState(false);
   const [message, setMessage] = useState("");
   const store = useContext(Context);
-  const [recorder, setRecorder] = useState<RecordRTC | null>(null);
   const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
-const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
+  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
+  const [volume, setVolume] = useState(0);
   const handleSend = () => {
     if (message.trim() !== "") {
       onSend(message, store.state.currentRoomId);
@@ -36,42 +34,53 @@ const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
   };
 
   const handleRecord = async () => {
-    if (isRecording) {
-      // Start recording
+    if (!isRecording) {
+      setAudioBlob(null);
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
       });
 
-      const options = {
-        type: 'audio' as "audio" | "video" | "canvas" | "gif" | undefined,
-        mimeType: "audio/webm" as "audio/webm" | "audio/webm;codecs=pcm" | "video/mp4" | "video/webm" | "video/webm;codecs=vp9" | "video/webm;codecs=vp8" | "video/webm;codecs=h264" | "video/x-matroska;codecs=avc1" | "video/mpeg" | "audio/wav" | "audio/ogg" | undefined,
-        recorderType: RecordRTC.StereoAudioRecorder,
-        numberOfAudioChannels: 1 as 1 | 2,
-      };
-
-      const newRecorder = new RecordRTC(stream, options);
-      newRecorder.startRecording();
-
+      if (!stream) {
+        store.displayError("Cannot access microphone");
+      }
       setIsRecording(true);
-      setRecorder(newRecorder);
+      const currentRecorder = new window.MediaRecorder(stream);
+
+      let blobData: Blob | null = null;
+      setRecorder(currentRecorder);
+      currentRecorder.ondataavailable = (event) => {
+        blobData = event.data;
+      }
+
+      currentRecorder.onstop = () => {
+        setIsRecording(false);
+        if (blobData) {
+          setAudioBlob(blobData);
+        }
+      }
+      currentRecorder.start();
     } else {
-      // Stop recording and send the audio file to the server
       const currentRecorder = recorder;
-
       if (currentRecorder) {
-        currentRecorder.stopRecording(() => {
-          setAudioBlob(currentRecorder.getBlob());
-        });
-
+        currentRecorder.stop();
+        currentRecorder.stream.getTracks().forEach((track) => track.stop());
         setRecorder(null);
       }
     }
   };
-  
-  useEffect(() => {
-    handleRecord();
-  }
-    , [isRecording]);
+
+  // useEffect(() => {
+  //   if (recorder) {
+  //     const interval = setInterval(() => {
+  //       setVolume(recorder.stream.getAudioTracks()[0].getSettings().volume);
+  //     }, 100);
+
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }
+  // }, [recorder]);
+
   return (
     <Box
       sx={{
@@ -82,39 +91,92 @@ const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
       }}
     >
       {!audioBlob ?
-        <TextField
-          fullWidth
-          maxRows={4}
-          multiline
-          value={message}
-          sx={{
-            "& .MuiInputBase-root": {
-              backgroundColor: "primary.main",
-            },
-          }}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyDown={handleKeyDown}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleSend}>
-                  <Send />
-                </IconButton>
-              </InputAdornment>
-            ),
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton onClick={
-                  () => {
-                    setIsRecording(!isRecording);
-                  }
-                }>
-                  <Mic />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
+        isRecording ? <Box sx={{
+          width: "100%",
+          boxSizing: "border-box",
+          display: "flex",
+          border: "1px solid black",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "primary.main",
+        }}>
+          <Box sx={{
+            height: "100%",
+            boxSizing: "border-box",
+
+          }}>
+
+            <IconButton
+              sx={
+                {
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                }
+              }
+              onClick={
+                handleRecord
+              }>
+              <Mic />
+            </IconButton>
+          </Box>
+          <Box sx={{
+            height: "100%",
+            boxSizing: "border-box",
+            display: "flex",
+            alignItems: "center",
+          }}>
+            <div className="meter">
+              <div className="meter__bar" />
+            </div>
+          </Box>
+          <Box
+            sx={{
+              height: "100%",
+              boxSizing: "border-box",
+              display: "flex",
+              alignItems: "center",
+
+            }}
+          >
+            <IconButton onClick={handleRecord}>
+              <Send />
+            </IconButton>
+          </Box>
+        </Box>
+          : (
+            <TextField
+              fullWidth
+              maxRows={4}
+              multiline
+              value={message}
+              sx={{
+                "& .MuiInputBase-root": {
+                  backgroundColor: "primary.main",
+                },
+              }}
+              onChange={(event) => setMessage(event.target.value)}
+              onKeyDown={handleKeyDown}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleSend}>
+                      <Send />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton onClick={
+                      handleRecord
+                    }>
+                      <Mic />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />)
         : (
           <Box sx={{
             width: "100%",
@@ -124,6 +186,7 @@ const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
             flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
+            backgroundColor: "primary.main",
           }}>
             <Box sx={{
               height: "100%",
@@ -140,9 +203,7 @@ const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
                   }
                 }
                 onClick={
-                  () => {
-                    setIsRecording(!isRecording);
-                  }
+                  handleRecord
                 }>
                 <Mic />
               </IconButton>
@@ -165,7 +226,7 @@ const [analyserNode, setAnalyserNode] = useState<AnalyserNode | null>(null);
               }}
             >
               <IconButton onClick={() => {
-                setAudioBlob(null);
+                handleRecord();
               }}>
                 <Send />
               </IconButton>
