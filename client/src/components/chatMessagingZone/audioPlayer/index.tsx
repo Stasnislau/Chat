8568d85d6d21a46724bc036d7a136eccd00f125b
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-10-07 18:15:22 +0200

    fixed slider

--- client/src/components/chatMessagingZone/audioPlayer/index.tsx
+++ client/src/components/chatMessagingZone/audioPlayer/index.tsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from 'react';
 import { Box, IconButton, Slider, Typography } from '@mui/material';
 import { PlayArrow, Pause } from '@mui/icons-material';
 
+
 const AudioPlayer = ({
   audioUrl: audioUrl,
 }:
@@ -12,6 +13,7 @@ const AudioPlayer = ({
   const audioPlayer = useRef<HTMLAudioElement>(null);
   const animationRef = useRef<number>(0);
   const [duration, setDuration] = useState<number>(0);
+  const [currentTime, setCurrentTime] = useState<number>(0);
 
   const calculateTime = (secs: number) => {
     const minutes = Math.floor(secs / 60);
@@ -22,55 +24,55 @@ const AudioPlayer = ({
   };
 
   const togglePlayPause = () => {
-    const audio = audioPlayer.current;
-    if (audio) {
-      const prevValue = isPlaying;
-      setIsPlaying(!prevValue);
-      if (!prevValue) {
-        audio.play();
-        animationRef.current = requestAnimationFrame(whilePlaying);
-      } else {
-        audio.pause();
-        cancelAnimationFrame(animationRef.current);
-      }
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current?.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    } else {
+      audioPlayer.current?.pause();
+      cancelAnimationFrame(animationRef.current);
     }
   };
 
   const whilePlaying = () => {
-    if (audioPlayer && audioPlayer.current) {
-      animationRef.current = requestAnimationFrame(whilePlaying);
+    if (audioPlayer.current?.duration) {
+      setCurrentTime(audioPlayer.current.currentTime);
+      setDuration(audioPlayer.current.duration);
     }
   };
 
   useEffect(() => {
-    if (audioPlayer && audioPlayer.current) {
-      audioPlayer.current.addEventListener('ended', () => {
-        setIsPlaying(false);
-        if (audioPlayer && audioPlayer.current) {
-          return;
-        }
-        if (audioPlayer.current === null) return;
-        audioPlayer.current.currentTime = 0;
-        cancelAnimationFrame(animationRef.current);
-      });
+    if (isPlaying) {
+      const intervalId = setInterval(whilePlaying, 50);
+      return () => clearInterval(intervalId);
+    } else {
+      cancelAnimationFrame(animationRef.current);
     }
-  }, [audioPlayer?.current?.addEventListener]);
+  }, [isPlaying]);
+
+
+  const handleSliderChange = (event: any, newValue: number | number[]) => {
+    if (audioPlayer.current) {
+      audioPlayer.current.currentTime = newValue as number;
+      setCurrentTime(newValue as number);
+    }
+  };
 
   useEffect(() => {
-    const audio = audioPlayer.current;
-    if (audio) {
-      const handleCanPlayThrough = () => {
-        audio.play();
-        audio.pause();
-        setDuration(audio.duration);
-      };
-      audio.addEventListener("canplaythrough", handleCanPlayThrough);
-      return () => {
-        audio.removeEventListener("canplaythrough", handleCanPlayThrough);
-        cancelAnimationFrame(animationRef.current);
-      };
-    }
-  }, [audioPlayer?.current?.addEventListener]);
+    audioPlayer.current?.addEventListener("ended", () => {
+      setIsPlaying(false);
+      setCurrentTime(0);
+    });
+    return () => {
+      audioPlayer.current?.removeEventListener("ended", () => {
+        setIsPlaying(false);
+        setCurrentTime(0);
+      });
+    };
+  }, []);
+
+
 
   return (
     <Box sx={
@@ -81,7 +83,11 @@ const AudioPlayer = ({
         maWidth: "300px",
       }
     }>
-      <audio ref={audioPlayer} preload="auto">
+      <audio ref={audioPlayer} preload="auto" onTimeUpdate={
+        () => {
+          setCurrentTime(prev => audioPlayer?.current?.currentTime ?? prev);
+        }
+      }>
         <source src={audioUrl} />
       </audio>
       <IconButton onClick={togglePlayPause}>
@@ -94,22 +100,16 @@ const AudioPlayer = ({
           marginRight: '10px',
         }
 
-      }>{calculateTime(Number(audioPlayer.current?.currentTime))}</Typography>
+      }>{calculateTime(currentTime)}</Typography>
       <Slider
         aria-label="time-indicator"
         size="small"
-        value={audioPlayer.current?.currentTime || 0}
+        value={currentTime}
         min={0}
-        step={(audioPlayer.current?.duration ? audioPlayer.current?.duration / 100 : 0)}
-        max={audioPlayer.current?.duration || 0}
-        onChange={(_, value) => {
-          console.log("Value: ", value)
-          if (audioPlayer && audioPlayer.current) {
-            audioPlayer.current.currentTime = Number(value);
-          }
-        }}
+        max={duration}
+        onChange={handleSliderChange}
         sx={{
-          color: 'red',
+          color: 'secondary.main',
           width: {
             mobile: '50px',
             tablet: '200px',
@@ -117,30 +117,16 @@ const AudioPlayer = ({
             desktop: '400px',
           },
           height: 4,
-          '& .MuiSlider-thumb': {
-            width: 8,
-            height: 8,
-            transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
-            '&:before': {
-              boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
-            },
-            '&.Mui-active': {
-              width: 20,
-              height: 20,
-            },
-          },
-          '& .MuiSlider-rail': {
-            opacity: 0.28,
-          },
+
         }}
       />
-
       <Typography sx={
         {
           fontSize: '16px',
           marginLeft: '10px',
         }
       }>{(duration && !isNaN(duration)) && calculateTime(duration)}</Typography>
+
     </Box >
   );
 };
