ddddec033d7735dd753e17d1984cfb8429174335
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-10-11 16:05:31 +0200

    integrated chosen users

--- client/src/components/Modals/createRoomModal/index.tsx
+++ client/src/components/Modals/createRoomModal/index.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, IconButton, Modal, Typography } from "@mui/material";
-import { useEffect, useState, useContext, useRef } from "react";
+import { Box, Button, Divider, IconButton, Modal, Skeleton, Typography } from "@mui/material";
+import { useEffect, useState, useContext } from "react";
 import { Context } from "../../../App";
 import LocalSearchField from "../localSearchField";
 import { user } from "../../../types";
+import UserBoxBadged from "../../userBoxBadged";
+import { API_URL } from "../../../constants";
 
 interface createRoomModalProps {
   isOpen: boolean,
@@ -14,10 +16,93 @@ const CreateRoomModal = (
     setIsOpen,
   }: createRoomModalProps) => {
   {
-
-    const [searchText, setSearchText] = useState("");
-    const [users, setUsers] = useState<user[]>([]);
     const store = useContext(Context);
+    const [searchText, setSearchText] = useState("");
+    const [selectedUserIds, setSelectedUserIds] = useState<string[]>([store.state.userId]);
+    const [chosenUsers, setChosenUsers] = useState<user[]>([]);
+    const [searchResults, setSearchResults] = useState<user[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    // фетчить новых юхеров по мере добавления их в чосенЮзерс и удалять если их убрали из списка айдишников
+
+    const fetchChosenUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `${API_URL}/user/getUsersByIds`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ids: selectedUserIds,
+            }),
+          }
+        );
+
+        const data = await response.json();
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(data.message);
+        }
+        setChosenUsers(data);
+      } catch (error: any) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    const fetchUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `${API_URL}/user/searchByNickname/${searchText}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(data.message);
+        }
+        setSearchResults(data);
+      } catch (error: any) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      const func = async () => {
+        if (selectedUserIds.length > 0) {
+          await fetchChosenUsers();
+        }
+      }
+      func();
+    }, [selectedUserIds.length]);
+
+    useEffect(() => {
+      const func = async () => {
+        if (
+          searchText !== "" && searchText.length > 2
+        ) {
+          await fetchUsers();
+        }
+      }
+      func();
+    }, [fetchUsers, searchText]);
+
+    useEffect(() => {
+      if (!searchText || searchText.length < 3) {
+        setSearchResults([]);
+      }
+    }, [searchText]);
+
     return (
       <Modal
         open={isOpen}
@@ -34,7 +119,7 @@ const CreateRoomModal = (
         <Box
           sx={{
             backgroundColor: "#FFFFFF",
-            height: "60%",
+            height: "70%",
             width: {
               mobile: "90%",
               tablet: "70%",
@@ -50,53 +135,178 @@ const CreateRoomModal = (
             p: 4,
           }}
         >
-          {/* Two levels: upper with the LocalSearchBar which search for people and a part for actually choosing whether we want 1 person in our chat or more.  */}
-          <Box sx={
+          <Typography sx={
             {
-              display: "flex",
-              height: "60%",
-              width: "100%",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              fontFamily: "Roboto",
+              fontSize: 20,
             }
-          }>
-            <Box sx={
-              {
-                display: "flex",
-                height: "30%",
-                width: "100%",
-                alignItems: "center",
-                justifyContent: "center",
-                fontFamily: "Roboto",
-              }
-            }
-            >
-              <LocalSearchField
-                searchText={searchText}
-                setSearchText={setSearchText}
-                key="search"
-              />
-            </Box>
+          } fontWeight="bold" textAlign="left" paddingBottom="1rem">
+            Create chat
+          </Typography>
+          <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+          >
             <Box sx={
               {
                 display: "flex",
                 width: "100%",
-                alignItems: "center",
-                justifyContent: "center",
-                fontFamily: "Roboto",
                 flexGrow: 1,
-                border: "1px solid black",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                fontFamily: "Roboto",
+              }
+            }>
+              <Box sx={
+                {
+                  display: "flex",
+                  width: "100%",
+                  height: "fit-content",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  fontFamily: "Roboto",
+                }
+              }
+              >
+                <LocalSearchField
+                  searchText={searchText}
+                  setSearchText={setSearchText}
+                  key="search"
+                />
+              </Box>
+              {
+                <Box sx={
+                  {
+                    display: "flex",
+                    width: "100%",
+                    justifyContent: "center",
+                    fontFamily: "Roboto",
+                    flexGrow: 1,
+                    borderTop: "1px solid black",
+                    borderBottom: "1px solid black",
+                    margin: "1rem 0 1rem 0",
+                    boxSizing: "border-box",
+                    overflowY: "auto",
+                  }
+                }
+                >
+                  {searchResults && searchResults.length > 0 && searchResults.map((user) => {
+                    return (
+                      <Box sx={
+                        {
+                          display: "flex",
+                          width: "100%",
+                          flexDirection: "row",
+                          fontFamily: "Roboto",
+                          flexWrap: "wrap",
+                        }
+                      }>
+                        <UserBoxBadged
+                          userId={user.id}
+                          name={user.name}
+                          avatar={user.avatar}
+                          isChosen={selectedUserIds.includes(user.id)}
+                          handleUserClick={(id: string) => {
+                            if (selectedUserIds.includes(user.id)) {
+                              if (selectedUserIds.length === 1) {
+                                return;
+                              }
+                              setSelectedUserIds(selectedUserIds.filter((userId) => userId !== user.id));
+                            } else {
+                              setSelectedUserIds([...selectedUserIds, user.id]);
+                            }
+                          }}
+                        />
+                      </Box>
+
+                    )
+                  })}
+                  {
+                    !searchResults || searchResults.length === 0  && chosenUsers && chosenUsers.length !== 0 && !isLoading && chosenUsers.map((user) => {
+                      return (
+                        <Box sx={
+                          {
+                            display: "flex",
+                            width: "100%",
+                            flexDirection: "row",
+                            fontFamily: "Roboto",
+                            flexWrap: "wrap",
+                          }
+                        }>
+                          <UserBoxBadged
+                            userId={user.id}
+                            name={user.name}
+                            avatar={user.avatar}
+                            isChosen={selectedUserIds.includes(user.id)}
+                            handleUserClick={(id: string) => {
+                              if (selectedUserIds.includes(user.id)) {
+                                if (selectedUserIds.length === 1) {
+                                  return;
+                                }
+                                setSelectedUserIds(selectedUserIds.filter((userId) => userId !== user.id));
+                              } else {
+                                setSelectedUserIds([...selectedUserIds, user.id]);
+                              }
+                            }}
+                          />
+                        </Box>
+                      )
+                    })
+                  }
+                  <Box sx={
+                    {
+                      display: selectedUserIds.length > 2 ? "flex" : "none",
+                    }}
+                  >
+
+                  </Box>
+                </Box>
+              }
+            </Box>
+
+            <Box sx={
+              {
+                display: "flex",
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                fontFamily: "Roboto",
               }
             }
             >
-              {/* TODO: Add the list of people that we can choose from. */}
-
+              <Button
+                sx={
+                  {
+                    display: "flex",
+                    width: "30%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontFamily: "Roboto",
+                    color: "black",
+                    variant: "contained",
+                    backgroundColor: "secondary.main",
+                    boxShadow: 4,
+                    "&:hover": {
+                      backgroundColor: "secondary.dark",
+                    },
+                  }
+                }
+                onClick={() => {
+                  setIsOpen(false);
+                }
+                }
+              >
+                Create
+              </Button>
             </Box>
           </Box>
         </Box>
-      </Modal >
+      </Modal>
     )
   }
 }
